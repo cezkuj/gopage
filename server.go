890bc422a7e1f6284057dcc82445f1117d08fd8f
cezkuj <cezkuj@gmail.com>
2018-04-19 19:31:41 +0000

    Hi React

---
+++ server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	cdnReact           = "https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react.min.js"
+	cdnReactDom        = "https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react-dom.min.js"
+	cdnBabelStandalone = "https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.24.0/babel.min.js"
+	cdnAxios           = "https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.1/axios.min.js"
+)
+
+const indexHTML = `
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hi React</title>
+  </head>
+  <body>
+    <div id='root'></div>
+    <script src="` + cdnReact + `"></script>
+    <script src="` + cdnReactDom + `"></script>
+    <script src="` + cdnBabelStandalone + `"></script>
+    <script src="` + cdnAxios + `"></script>
+    <script src="/js/app.jsx" type="text/babel"></script>
+  </body>
+</html>
+`
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, indexHTML)
+}
+func main() {
+	serveMux := &http.ServeMux{}
+	serveMux.HandleFunc("/", HelloServer)
+	serveMux.Handle("/js/", http.FileServer(http.Dir("assets")))
+	srv := &http.Server{
+		Addr:         ":8000",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Handler:      serveMux,
+	}
+	log.Println(srv.ListenAndServe())
+}
