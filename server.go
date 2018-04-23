975561ff68a0c807def6e95782a11b7168cbe6fb
cezkuj <cezkuj@gmail.com>
2018-04-23 11:48:29 +0000

    Added handlig for cookies

--- server.go
+++ server.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+type Env struct{}
+
 const (
 	cdnReact           = "https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react.min.js"
 	cdnReactDom        = "https://cdnjs.cloudflare.com/ajax/libs/react/15.5.4/react-dom.min.js"
@@ -32,19 +34,53 @@ const indexHTML = `
 </html>
 `
 
+func (env Env) isAuthenticated(username, token string) (bool, error) {
+	log.Println(username, token)
+	return true, nil
+
+}
+func authenticate(env Env) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, err := r.Cookie("username")
+		if err != nil {
+			io.WriteString(w, "Not authenticated")
+			return
+		}
+		token, err := r.Cookie("token")
+		if err != nil {
+			io.WriteString(w, "Not authenticated")
+			return
+		}
+
+		log.Println(username.Value, token.Value)
+		authenticated, err := env.isAuthenticated(username.Value, token.Value)
+		if !authenticated || err != nil {
+			io.WriteString(w, "Not authenticated")
+		} else {
+			io.WriteString(w, "Authenticated")
+		}
+
+	}
+}
+
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	expiration := time.Now().Add(365 * 24 * time.Hour)
-	cookie := http.Cookie{Name: "username", Value: "cezkuj", Expires: expiration}
-	http.SetCookie(w, &cookie)
+	nextDay := time.Now().Add(24 * time.Hour)
+	midnight := time.Date(nextDay.Year(), nextDay.Month(), nextDay.Day(), 0, 0, 0, 0, nextDay.Location())
+	cookieUsername := http.Cookie{Name: "username", Value: "cezkuj", Expires: midnight}
+	cookieToken := http.Cookie{Name: "token", Value: "secr3t", Expires: midnight}
+	http.SetCookie(w, &cookieUsername)
+	http.SetCookie(w, &cookieToken)
 	for _, cookie := range r.Cookies() {
 		log.Println(w, cookie.Name)
 	}
 	io.WriteString(w, indexHTML)
 }
 func main() {
+	env := Env{}
 	serveMux := &http.ServeMux{}
 	serveMux.HandleFunc("/", HelloServer)
 	serveMux.Handle("/js/", http.FileServer(http.Dir("assets")))
+	serveMux.HandleFunc("/authenticate", authenticate(env))
 	srv := &http.Server{
 		Addr:         ":8000",
 		ReadTimeout:  5 * time.Second,
